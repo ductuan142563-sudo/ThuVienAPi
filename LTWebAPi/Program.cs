9f2cd3aa57d2c37e183a5c7873cf0c2a0de3a922
Ketnoicuuho <ductuan142563@gmail.com>
2026-09-24 14:40:25 +0700

    Triển khai cơ chế Asynchronous và Reposity Pattern

--- LTWebAPi/Program.cs
+++ LTWebAPi/Program.cs
@@ -1,5 +1,6 @@
 using LTWebAPi.Data;
 using Microsoft.EntityFrameworkCore;
+using WebAPI_simple.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddSwaggerGen();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IBookRepository, SQLBookRepository>();
 
 var app = builder.Build();
 
---
+++ LTWebAPi/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using LTWebAPi.Models.Domain;
+using LTWebAPi.Models.DTO;
+namespace WebAPI_simple.Repositories
+{
+    public interface IBookRepository
+    {
+        List<BookWithAuthorAndPublisherDTO> GetAllBooks();
+        BookWithAuthorAndPublisherDTO GetBookById(int id);
+        AddBookRequestDTO AddBook(AddBookRequestDTO addBookRequestDTO);
+        AddBookRequestDTO? UpdateBookById(int id, AddBookRequestDTO bookDTO);
+        Book? DeleteBookById(int id);
+    }
+}
